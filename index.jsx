3f2615493dff38563e01aaa8af5fdea823851e3a
Asif Afghan <masifafghan017@gmail.com>
2026-05-04 23:50:16 +0500

    post method

--- index.jsx
+++ index.jsx
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = 8050;
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
-
+app.post('/api/data', (req, res)=>{
+    res.send('data recevied');
+})
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
