93fbf711a59c82a0ba0c5a60c98db237f024240e
Asif Afghan <masifafghan017@gmail.com>
2026-05-05 23:38:29 +0500

    install and connect the Mongoose

--- index.jsx
+++ index.jsx
@@ -1,6 +1,13 @@
 const express = require('express');
+const { mongo } = require('mongoose');
 const app = express();
 const port = 8050;
+
+mongoose.connect('mongodb://localhost:27017/User-data')
+.then(() => {
+  console.log('Connected to MongoDB');
+});
+
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
@@ -11,3 +18,9 @@ app.post('/api/data', (req, res)=>{
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
+const userSchema =new mongoose.Schema({
+  firstName:{ type: String, required: true },
+  lastName:{ type: String, required: true },
+  email:{ type: String, required: true, unique: true }
+})
+const User = mongoose.model('User', userSchema);
