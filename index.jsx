a1aa948774722ec4d0aa514230d6a569694fbaad
Asif Afghan <masifafghan017@gmail.com>
2026-05-06 23:36:57 +0500

    catch

--- index.jsx
+++ index.jsx
@@ -7,9 +7,6 @@ mongoose.connect('mongodb://localhost:27017/User-data')
 .then(() => {
   console.log('Connected to MongoDB');
 })
-.catch((error) => {
-  console.error('Error connecting to MongoDB:', error);
-});
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
